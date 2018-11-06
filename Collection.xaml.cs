30441ebf88acaed3ee91bbaec8f12143ce381587
Noah Osterhout <osterh3@ad.nmc.edu>
2018-11-06 14:50:55 -0500

    Added Projct Files

---
+++ Collection.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_Binding
+{
+    /// <summary>
+    /// Interaction logic for Collection.xaml
+    /// </summary>
+    public partial class Collection : Window
+    {
+        private ObservableCollection<Person2> _peopleList = new ObservableCollection<Person2>();
+
+        public Collection()
+        {
+            InitializeComponent();
+            _peopleList = new ObservableCollection<Person2>
+            {
+                new Person2 { Name = "Noah", Age = 19},
+                new Person2 { Name = "Keegan", Age = 19},
+                new Person2 { Name = "Richie", Age = 19},
+                new Person2 { Name = "Eric", Age = 18}
+            };
+            _list.ItemsSource = _peopleList;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            _peopleList.Add(new Person2 { Name="Wyatt", Age = 19});
+        }
+
+        private void close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Binding
+{
+    public class Person : INotifyPropertyChanged
+    {
+        public string Name { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        int _age;
+        
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                _age = value;
+                var pc = PropertyChanged;
+                if (pc != null)
+                {
+                    pc(this, new PropertyChangedEventArgs("Age"));
+                }
+            }
+        }
+    }
+}
